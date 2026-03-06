675d20064dadf54e90ba0447a879b0ff203c4403
Shouri Chatterjee <shouri@ee.iitd.ac.in>
2026-03-06 20:26:35 +0530

    Initial commit

---
+++ examsched.php
@@ -0,0 +1,9 @@
+<?php
+$exams[1] = array('A', ', all other');
+$exams[2] = array('B', ', all other');
+$exams[3] = array('C', ', all other');
+$exams[4] = array('D', ', all other');
+$exams[5] = array('E', ', all other');
+$exams[6] = array('F', ', all other');
+$exams[7] = array('all other');
+?>
